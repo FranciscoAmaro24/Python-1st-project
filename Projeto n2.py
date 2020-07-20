9ffa2869f99e7337fab88a8fa3366ff7e23a3d70
FranciscoAmaro24 <66916703+FranciscoAmaro24@users.noreply.github.com>
2020-07-20 16:52:30 +0100

    Add files via upload

---
+++ Projeto n2.py
@@ -0,0 +1,7 @@
+rice = 1
+total = 0
+for i in range(1,65):
+    total += rice
+    print ('The amount of rice on squase %s is: %s'%(i,rice))
+    print('out total amount of rice is: %s' %(total))
+    rice = rice * 2
